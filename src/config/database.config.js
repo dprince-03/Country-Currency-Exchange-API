b514da33d95424cf4348a97ad96adbd8b32d2937
D'Prince <princeadedayo03@gmail.com>
2025-10-28 19:04:48 +0100

    HNG 13 - stage 2

--- src/config/database.config.js
+++ src/config/database.config.js
@@ -5,7 +5,7 @@ const dbconfig = {
 	host: process.env.DB_HOST || "localhost",
 	user: process.env.DB_USER || "root",
 	password: process.env.DB_PASSWORD || "",
-	database: process.env.DB_NAME || "",
+	database: process.env.DB_NAME || "country_currency_db",
 	port: process.env.DB_PORT || 3306,
 	waitForConnections: true,
 	connectionLimit: 50,
